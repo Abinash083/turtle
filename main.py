0f7e9f553ff4bd87c5c5cc93aeb6d9cb1791ee86
Abinash <avinashniure026@gmail.com>
2022-02-21 16:13:18 +0930

    turtle listen demo

--- main.py
+++ main.py
@@ -21,10 +21,17 @@ def move_back():
     t1.forward(-10)
 
 
+def reset():
+    t1.home()
+    t1.clear()
+
+
+
+screen.listen()
 turtle.onkeypress(fun=move_forward, key="w")
 turtle.onkeypress(fun=rotate_left, key="a")
 turtle.onkeypress(fun=rotate_right,key="d")
 turtle.onkeypress(fun=move_back,key="s")
+turtle.onkeypress(fun=reset,key="c")
 
-screen.listen()
 screen.exitonclick()
