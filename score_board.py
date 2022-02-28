2f336f8f9fd13d696f707f8e0daddfad096bff9e
Abinash <avinashniure026@gmail.com>
2022-02-28 21:06:01 +0930

    high score concept in snake game

--- score_board.py
+++ score_board.py
@@ -4,6 +4,7 @@ from turtle import Turtle
 class Board(Turtle):
     def __init__(self, x, y):
         super().__init__()
+        self.high_score = 0
         self.score = 0
         self.penup()
         self.pencolor("black")
@@ -11,6 +12,7 @@ class Board(Turtle):
         self.hideturtle()
         self.goto(self.x, self.y)
         self.write_score()
+        self.determide_high_score()
 
     def write_score(self):
         self.write(f"Score {self.score}", align="center", font=("arial", 13, "normal"))
@@ -24,3 +26,15 @@ class Board(Turtle):
         self.goto(x, y)
         self.color("red")
         self.write(f"GAME OVER", align="center", font=("Courier'", 16, "bold"))
+        self.goto(x, y-20)
+        self.determide_high_score()
+        self.write(f"Highest Score : {self.high_score}", align="center", font=("Courier'", 12, "bold"))
+
+    def determide_high_score(self):
+        with open("high_score.txt", mode="r") as file:
+            self.high_score = file.read()
+
+        if self.score > int(self.high_score):
+            self.high_score = str(self.score)
+            with open("high_score.txt", mode="w") as file:
+                file.write(self.high_score)
